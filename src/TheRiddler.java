e88bdb4fcd8ce0a747b51fc84ff8f016487cb76a
Syu125 <yusophia2011@gmail.com>
2017-11-14 19:24:23 -0800

    UNBirthday

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -8,7 +8,7 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+ int score=
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
 
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
---
+++ src/UNBirthday.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+public class UNBirthday {
+
+	public static void main(String[] args) {
+		String Today = "11/14/17";
+		String Birthday =JOptionPane.showInputDialog("When is your birthday?");
+		if(Today.equals(Birthday)){
+		JOptionPane.showMessageDialog(null,"Happy Birthday");
+	}else{JOptionPane.showMessageDialog(null, "Happy Unbirthday");
+		JOptionPane.showMessageDialog(null, "Happy Unbirthday");
+		
+		}
+		
+		
+		
+		
+	}  
+	
+	
+	
+	
+	}
